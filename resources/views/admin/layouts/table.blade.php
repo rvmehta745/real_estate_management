e1237fcd1f0a2abd9ec56d12415b27c083788050
Ritesh Patel <rp214322@gmail.com>
2022-12-30 23:21:43 +0530

    commit

--- resources/views/admin/layouts/table.blade.php
+++ resources/views/admin/layouts/table.blade.php
@@ -64,7 +64,43 @@
 		<div class="main-container">
 			<div class="pd-ltr-20 xs-pd-20-10">
 				<div class="min-height-200px">
-					@include('admin.shared.main-container')
+					<div class="page-header">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-12">
+                                <div class="title">
+                                    <h4>DataTable</h4>
+                                </div>
+                                <nav aria-label="breadcrumb" role="navigation">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item">
+                                            <a href="{{ route('dashboard') }}">Home</a>
+                                        </li>
+                                        <li class="breadcrumb-item active" aria-current="page">
+                                            DataTable
+                                        </li>
+                                    </ol>
+                                </nav>
+                            </div>
+                            <div class="col-md-6 col-sm-12 text-right">
+                                <div class="dropdown">
+                                    <a
+                                        class="btn btn-primary dropdown-toggle"
+                                        href="#"
+                                        role="button"
+                                        data-toggle="dropdown"
+                                    >
+                                        January 2022
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right">
+                                        <a class="dropdown-item" href="#">Export List</a>
+                                        <a class="dropdown-item" href="#">Policies</a>
+                                        <a class="dropdown-item" href="#">View Assets</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
 					<!-- Simple Datatable start -->
 					<div class="card-box mb-30">
 						<div class="pd-20">
