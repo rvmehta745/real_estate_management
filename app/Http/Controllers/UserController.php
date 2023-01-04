80f9fe69b8a8b3fccfa2300dd392f18105a51598
Ritesh Patel <rp214322@gmail.com>
2023-01-04 11:25:36 +0530

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,8 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users=User::all();
-        return view('admin.users.index',compact('users'));
+        return view('admin.datatables.users.index');
     }
 
     /**
